58b2bf61e20d8e68558687ce29aca4d74665132a
mariana <103067658+litzaedit@users.noreply.github.com>
2024-07-24 10:20:41 -0300

    .

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", funcion () => respostaSelecionada(alternativa));
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
