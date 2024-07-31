865996660b028c39fb19d789a2b9435b063af48d
Daniely Vitoria Dallagrana <103067233+yyy1212@users.noreply.github.com>
2024-07-31 10:03:07 -0300

    .

--- script.js
+++ script.js
@@ -6,56 +6,42 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Quando voce ve reportagens, notícias sobre como o ecossistema vem sendo degradado, voce pensa em como pode ajudar?",
+        enunciado: "Quando voce ve reportagens, noticias sobre como o ecossistema vem sendo degradado, voce pensa em como pode ajudar?",
         alternativas: [
-            {    
+            {
                 texto: "Sim, eu tenho ações para amenizar",
-                afirmação: "afirmação"
-              
+                afirmacao: "afirmação"
             },
-
             {
                 texto: "Eu não sei o que poderia fazer",
-                afirmação: "afirmação"
-
+                afirmacao: "afirmação"
             }
-            
         ]
     },
-  
     {
         enunciado: "Estão aqui duas opções para voce, que tal por enquanto começar com uma dessas?",
         alternativas: [
-           {
-               texto: "Economizar energia (quando for utilizar o celular pode desligar a tv, diminuir o tempo do chuveiro, são exemplos)",
-               afirmação: "afirmação"
-
-           },
-            
-        {
-              texto: "Descartar corretamente o lixo (não jogar na rua, terrenos baldios, separar o lixo em casa, também são exemplos)",
-              afirmação: "afirmação"
-
-        }
-           
-           
+            {
+                texto: "Economizar energia (quando for utilizar o celular pode desligar a tv, diminuir o tempo do chuveiro, são exemplos)",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Descartar corretamente o lixo (não jogar na rua, terrenos baldios, separar o lixo em casa, também são exemplos)",
+                afirmacao: "afirmação"
+            }
         ]
     },
-  
     {
         enunciado: "São algumas dicas de saúde, que tal escolher uma delas?",
         alternativas: [
-         {
-             texto: "Horta caseira. Voce saiba que a horta caseira proporciona uma alimentação mais saudável e funciona como terapia para a mente já que o contato com a terra ajuda no controle do estresse e ansiedade",
-             afirmação: "afirmação"
-
-         }
-           
-         {
-            texto: "Compostagem. A compostagem ajuda na redução de CO2, melhora a qualidade do solo e também ajuda a reduzir a dependencia de fertilizantes químicos. Algumas empresas: Retec Resíduos - Composta Mais - Grupo Carbono"
-            afirmação: "afirmação"
-
-         }
+            {
+                texto: "Horta caseira. Voce saiba que a horta caseira proporciona uma alimentação mais saudável e funciona como terapia para a mente já que o contato com a terra ajuda no controle do estresse e ansiedade.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Compostagem. A compostagem ajuda na redução de CO2, melhora a qualidade do solo e também ajuda a reduzir a dependencia de fertilizantes químicos. Algumas empresas: Retec Resíduos - Composta Mais - Grupo Carbono".",
+                afirmacao: "afirmação"
+            }
         ]
     },
     {
@@ -80,8 +66,13 @@ let perguntaAtual;
 let historiaFinal = "";
 
 function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 
@@ -94,11 +85,17 @@ function mostraAlternativas(){
     }
 }
 
-function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionada.afirmacoes;
-    historiaFinal = afirmacoes;
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
 }
 
 mostraPergunta();
